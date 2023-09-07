a0645f28ad9defcfdf7200033bfa64a670cc9c14
JRojas <jrojas@passporttechnology.com>
2023-09-07 17:42:32 -0500

    Problem 2 updates

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repository contains my answers to the Clojure Challenge described in the `p
 
 2. **Problem 2**: In `problem2.clj`, I had to copy the specs into the same source code file. I attempted to import them from the original file provided in the challenge (`invoice_spec.clj`), but the compiler didn't recognize the specs. By pasting them into the same file, I was able to successfully run the program with recognized specs.
 
-3. **Problem 2 Invoice**: It's important to note that the given invoice for problem 2 (`invoice.json`) does not meet the specified requirements of the given spec.
+3. **Problem 2 Invoice**: It's important to note that the given invoice for problem 2 (`invoice.json`) does not meet the specified requirements of the given spec. So, I read the JSON file and construct a map that pass the specs, doing the necessary changes to the original map extracted from the JSON. 
 
 4. **Problem 3 Tests**: I have written tests for `problem3.clj`, but I encountered an error every time I tried to execute the program. The error message was: "Could not locate `clojure/test'__init.class`, `clojure/test'.clj`, or `clojure/test'.cljc` on the classpath." This issue is unusual because I could locate the `clojure/test.clj` file in the project packages, and the compiler recognized it. However, during execution, the program encountered this error. I did not find a documented solution for this issue in the available documentation or forums. Nevertheless, I have written the tests and gained a good understanding of Clojure's testing features.
 
--- src/problem2.clj
+++ src/problem2.clj
@@ -1,6 +1,8 @@
 (ns problem2
   (:require [cheshire.core :as json]
             [clojure.spec.alpha :as s]
+            [clojure.walk :refer [postwalk]]
+            [clj-time.format :as tf]
             ))
 
 
@@ -28,13 +30,14 @@
                 :invoice-item/sku
                 :invoice-item/taxes]))
 
-(s/def :invoice/issue-date string?)
+(s/def :invoice/issue-date inst?)
 (s/def :invoice/items (s/coll-of ::invoice-item :kind vector? :min-count 1))
 
 (s/def ::invoice
   (s/keys :req [:invoice/issue-date
                 :invoice/customer
                 :invoice/items]))
+
 
 
 
@@ -46,31 +49,84 @@
         (fn [errors [k v]]
           (if (sequential? v)
             (into errors (map (fn [nested-error]
-                                (str k " " (custom-explain-data (s/get-spec k spec) nested-error)))
+                                (str k " " (custom-explain-data (s/get-spec k) nested-error)))
                               v))
             (conj errors (str k " " v))))
         []
         explanation)
       nil)))
 
-(defn read-and-validate-invoice [file-name]
-  ;; Step 1: Read the JSON file using slurp
-  (let [json-data (json/parse-string (slurp file-name) true)]
-    ;; Step 2: Define the spec and validate the parsed data
-    (let [invoice-spec ::invoice
-          validation-result (s/valid? invoice-spec json-data)]
-      (if validation-result
-        ;; Step 3: If valid, return the invoice
-        json-data
-        ;; Step 4: If invalid, handle validation errors
-        (do
-          (println "Validation Errors:")
-          (doseq [error (custom-explain-data invoice-spec json-data)]
-            (println error))
-          nil)))))
 
-(def invoice (read-and-validate-invoice "invoice.json"))
 
-(if invoice
-  (println invoice)
-  (println "Invoice is invalid."))
+
+(defn modify-json [data]
+  (postwalk
+    (fn [item]
+      ;;(println item)
+      (cond
+        (and (map? item) (:issue_date item) (:customer item) (:items item)
+             (:payment_means_type item) (:order_reference item) (:number item) (:payment_date item)
+             (:payment_means item) (:retentions item))
+        (-> item
+            (dissoc :issue_date)
+            (assoc :invoice/issue-date (tf/parse (tf/formatter "dd/MM/yyyy") (:issue_date item)))
+            (dissoc :customer)
+            (assoc :invoice/customer (:customer item))
+            (dissoc :items)
+            (assoc :invoice/items (:items item))
+            (dissoc :payment_means_type)
+            (assoc :invoice/payment_means_type (:payment_means_type item))
+            (dissoc :order_reference)
+            (assoc :invoice/order_reference (:order_reference item))
+            (dissoc :number)
+            (assoc :invoice/number (:number item))
+            (dissoc :payment_date)
+            (assoc :invoice/payment_date (:payment_date item))
+            (dissoc :payment_means)
+            (assoc :invoice/payment_date (:payment_date item))
+            (dissoc :retentions)
+            (assoc :invoice/retentions (:retentions item)))
+        (and (map? item) (:price item) (:quantity item) (:sku item) (:taxes item))
+        (-> item
+            (dissoc :price)
+            (assoc :invoice-item/price (:price item))
+            (dissoc :quantity)
+            (assoc :invoice-item/quantity (:quantity item))
+            (dissoc :sku)
+            (assoc :invoice-item/sku (:sku item))
+            (dissoc :taxes)
+            (assoc :invoice-item/taxes (:taxes item)))
+        (and (map? item) (:tax_category item) (:tax_rate item))
+        (-> item
+            (dissoc :tax_category)
+            (assoc :tax/category :iva)
+            (dissoc :tax_rate)
+            (assoc :tax/rate (double (:tax_rate item))))
+        (and (map? item) (:company_name item) (:email item))
+        (-> item
+            (dissoc :company_name)
+            (assoc :customer/name (:company_name item))
+            (dissoc :email)
+            (assoc :customer/email (:email item)))
+        :else
+        item))
+    data))
+
+
+
+
+
+(defn process [file-name]
+  (let [json (json/parse-string (slurp file-name) true)  modified-json (modify-json (get json :invoice))]
+    modified-json)
+  )
+
+(def new-invoice (process "invoice.json"))
+(println new-invoice)
+
+(def valid (s/valid? ::invoice new-invoice))
+(println valid)
+
+
+
+
